39ccdc15f632f135f8faaf597a07e2ecab01c17c
GHowlet1 <Griffin.Howlett1@T-Mobile.com>
2018-02-06 13:27:21 -0500

    __main__ interface returns results via exit code

--- gib_detect.py
+++ gib_detect.py
@@ -3,6 +3,7 @@
 import pickle
 import gib_detect_train
 import os
+import sys
 
 gib_model_file = os.path.join(os.path.dirname(__file__),'gib_model.pki')
 if not os.path.isfile(gib_model_file): gib_detect_train.train()
@@ -14,6 +15,8 @@ def is_gibberish(string):
     return gib_detect_train.avg_transition_prob(string, model_mat, threshold) <= threshold
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1: sys.exit(not is_gibberish(sys.argv[1]))
+    
     print('threshold: ' + str(threshold))
     while True:
         input = raw_input()
